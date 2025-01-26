bf89c18733454ef3f3dc8f0c76e80ebf1f031d55
Tommaso Negri <tommaso@hey.com>
2025-01-26 10:49:17 +0100

    Cleanup configuration

--- nvim/lua/plugins/lazy/chatgpt.lua
+++ nvim/lua/plugins/lazy/chatgpt.lua
@@ -1,13 +1,12 @@
 return {
 	"jackMort/ChatGPT.nvim",
-	event = "VeryLazy",
-	-- cmd = {
-	-- 	"ChatGPT",
-	-- 	"ChatGPTActAs",
-	-- 	"ChatGPTCompleteCode",
-	-- 	"ChatGPTEditWithInstructions",
-	-- 	"ChatGPTRun"
-	-- },
+	cmd = {
+		"ChatGPT",
+		"ChatGPTActAs",
+		"ChatGPTCompleteCode",
+		"ChatGPTEditWithInstructions",
+		"ChatGPTRun"
+	},
 	dependencies = {
 		"MunifTanjim/nui.nvim",
 		"nvim-lua/plenary.nvim",
@@ -28,16 +27,11 @@ return {
 		{ "<leader>cs", "<cmd>ChatGPTCompleteCode<CR>", desc = "ChatGPT Suggest completion" },
 		{ "<leader>ce", "<cmd>ChatGPTEditWithInstructions<CR>", desc = "ChatGPT Edit with instruction", mode = { "n", "v" } },
 
-		-- { "<leader>crg", "<cmd>ChatGPTRun grammar_correction<CR>", desc = "ChatGPT Grammar Correction", mode = { "n", "v" } },
-		-- { "<leader>crt", "<cmd>ChatGPTRun translate<CR>", desc = "ChatGPT Translate", mode = { "n", "v" } },
-		-- { "<leader>crk", "<cmd>ChatGPTRun keywords<CR>", desc = "ChatGPT Keywords", mode = { "n", "v" } },
 		{ "<leader>crd", "<cmd>ChatGPTRun docstring<CR>", desc = "ChatGPT Docstring", mode = { "n", "v" } },
 		{ "<leader>crt", "<cmd>ChatGPTRun add_tests<CR>", desc = "ChatGPT Add Tests", mode = { "n", "v" } },
 		{ "<leader>cro", "<cmd>ChatGPTRun optimize_code<CR>", desc = "ChatGPT Optimize Code", mode = { "n", "v" } },
 		{ "<leader>crs", "<cmd>ChatGPTRun summarize<CR>", desc = "ChatGPT Summarize", mode = { "n", "v" } },
 		{ "<leader>crf", "<cmd>ChatGPTRun fix_bugs<CR>", desc = "ChatGPT Fix Bugs", mode = { "n", "v" } },
 		{ "<leader>crx", "<cmd>ChatGPTRun explain_code<CR>", desc = "ChatGPT Explain Code", mode = { "n", "v" } },
-		-- { "<leader>crr", "<cmd>ChatGPTRun roxygen_edit<CR>", desc = "ChatGPT Roxygen Edit", mode = { "n", "v" } },
-		-- { "<leader>crl", "<cmd>ChatGPTRun code_readability_analysis<CR>", desc = "ChatGPT Code Readability Analysis", mode = { "n", "v" } },
 	}
 }
